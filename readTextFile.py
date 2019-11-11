527a17456ec4102e551f17e966d7003a7e243408
kory0005 <kory0005@algonquinlive.com>
2019-11-11 08:57:29 -0600

    adding all files to the repository

--- readTextFile.py
+++ readTextFile.py
@@ -7,7 +7,7 @@ def readTextFile(name):
     while (moveOn == 1):
         for line in fin:
             c = line[-1]
-            print(line)
+            print(list(line))
             moveOn = 0
         lc = lc + 1
 
