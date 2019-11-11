40e0b9fbd12cbf795141dc193fab61dba51a65a0
kory0005 <kory0005@algonquinlive.com>
2019-11-11 09:13:33 -0600

    adding all files to the repository

--- task2.py
+++ task2.py
@@ -3,17 +3,18 @@ import csv
 
 fileName = input('Enter a file name: ')
 
-fin = open(fileName, "r")
-lc = 1
-moveOn = 1
-while (moveOn == 1):
-    for line in fin:
-        c = line[-1]
-        # print(line)
-        print(list(line))
-        moveOn = 0
-    lc = lc + 1
+def readTextFile(name):
+    fin = open(name, "r")
+    lc = 1
+    moveOn = 1
+    while (moveOn == 1):
+        for line in fin:
+            c = line[-1]
+            # print(line)
+            print(list(line))
+            moveOn = 0
+        lc = lc + 1
 
-fin.close()
+    fin.close()
 
 readTextFile(fileName)
